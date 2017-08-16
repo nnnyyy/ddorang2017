b12777da82480db1612b8b69d0a088d6f097b210
hdpc-win10 <hungrysoul9@gmail.com>
2017-08-16 23:08:37 +0900

    Index 페이지 구현 - Desktop 에서도 보이도록, 일단 이거라도...

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var pool = require('../mysql/_Mysql').init();
+var async = require('async');
+var database = require('./database.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    try {
+        async.waterfall([
+            function(cb) {
+                cb(null, pool, {});
+            },
+
+            database.cb_sex_avg,
+            database.cb_top_avg,
+            database.cb_ranking_maximum,
+
+            function(pool, data, cb) {
+                data['rank_maximum'] = data['rank_maximum'].slice(0, 5);
+                cb(null, data);
+            }
+        ], function(err, ret) {
+            res.render('index', {data: ret, session: req.session.user_id });
+        });
+    }
+    catch(err) {
+        res.render('index', { session: req.session.user_id });
+    }
 });
 
 module.exports = router;
+
+
